68d4686b6cd7f54f943e65093f9d983b377887ee
edyting <dankyiemmanuel0@gmail.com>
2024-11-14 14:04:39 +0000

    adding frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Counter } from './features/counter/Counter';
+
+
+function App() {
+  return (
+    <div >
+      <h2>Hello</h2>
+    </div>
+  );
+}
+
+export default App;
